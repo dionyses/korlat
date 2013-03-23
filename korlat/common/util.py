8157c46322f4683043f463aebaa9068c615626c0
Lindsey Sawatzky <dionyses@gmail.com>
2013-03-23 15:42:36 -0600

    Rename canonical to canonical_class.

--- korlat/common/util.py
+++ korlat/common/util.py
@@ -1,21 +1,21 @@
-def canonical(obj):
-    """Get the canonical name of the object
+def canonical_class(obj):
+    """Get the canonical class name of the object
 
     >>> s = "stringy"
-    >>> canonical(s)
+    >>> canonical_class(s)
     object.basestring.str
     >>> c = Checkbox(my_web_app, ID, "yadda")
-    >>> canonical(c)
+    >>> canonical_class(c)
     object.Element.CheckableElement.AestheticElement.Checkbox
 
-    :param obj: the instance to retrieve the canonical name from
+    :param obj: the instance to retrieve the canonical class name from
     :type obj: object
     :returns: a dot (.) delimited string which absolutely identifies the inheritance chain of the specified object
     """
     if obj.__class__ != object.__class__:
-        return canonical(obj.__class__)
+        return canonical_class(obj.__class__)
     elif obj.__base__ is not None:
-        return "%s.%s" % (canonical(obj.__base__), obj.__name__)
+        return "%s.%s" % (canonical_class(obj.__base__), obj.__name__)
     else:
         return obj.__name__
 
--- korlat/tests/unit/util.py
+++ korlat/tests/unit/util.py
@@ -5,25 +5,25 @@ from korlat.abstraction.element import Element
 from korlat.core.strategy import ID
 from korlat.core.webapp import WebApp
 from korlat.common.objects import Checkbox
-from korlat.common.util import canonical
+from korlat.common.util import canonical_class
 
 
 class Tests(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_canonical_name(self):
+    def test_canonical_class_name(self):
         w = Mock()
         w.__class__ = WebApp
 
         e = Element(w, ID, "")
-        self.assertEqual(canonical(e), "object.Element")
+        self.assertEqual(canonical_class(e), "object.Element")
 
         c = Checkbox(w, ID, "")
-        self.assertEqual(canonical(c), "object.Element.CheckableElement.AestheticElement.Checkbox")
+        self.assertEqual(canonical_class(c), "object.Element.CheckableElement.AestheticElement.Checkbox")
 
         s = ""
-        self.assertEqual(canonical(s), "object.basestring.str")
+        self.assertEqual(canonical_class(s), "object.basestring.str")
 
 
 def suite():
